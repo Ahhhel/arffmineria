7a8e29f3015ac1a37fcb5f6b9e5c7d2c2a0657bb
Ahhhel <xxhercoxx@gmail.com>
2020-01-16 18:31:22 -0600

    Ya genera el .arff pero namas del servo.names unu

--- Programa.py
+++ Programa.py
@@ -1,3 +1,4 @@
+# names=open("shuttle-landing-control.names","tr")
 names=open("servo.names","tr")
 
 lineas=names.readlines()
@@ -5,23 +6,58 @@ lineas=names.readlines()
 l1=lineas[0].split(": ")
 nombreRelacion = l1[1]
 nombreRelacion =  nombreRelacion[:-1]
-print("@relation '"+ nombreRelacion+"'\n")
 
-numAtributosmin = 0
+
+numAtributos = 0
 for i in lineas:
-    numAtributosmin+=1
+    numAtributos+=1
     if (i[0] == "7"):
         break
-numAtributosmax = 0
-for i in lineas:
-    numAtributosmax+=1
-    if (i[0] == "8"):
+
+
+k=0
+
+atributos = {}
+
+while True:
+    if (lineas[numAtributos][0] == "8"):
         break
-print(numAtributosmin)
-print(" ")
-print(numAtributosmax)
+    if (lineas[numAtributos] != "\n"):
+        k+=1
+        atributos[k]=lineas[numAtributos].split(": ")
+    numAtributos+=1
+
+
+listaAtributos = {}
+
+j=0
+
+for atributo in atributos:
+    j+=1
+    nombre= atributos[atributo][0].split(". ")
+    detalles = atributos[atributo][1]
+    detalles = detalles[:-1]
+    if (" " in detalles):
+        listaAtributos[j]="@attribute "+nombre[1]+" NUMERIC"
+    else:
+        listaAtributos[j]="@attribute "+nombre[1]+" {"+detalles+"}"
+    
 
 
 
 
+
+inicio= "@relation '"+ nombreRelacion+"'\n\n"
+
+for l in listaAtributos:
+    print(listaAtributos[l])
+
+archivoarff = open(nombreRelacion+".arff","w")
+
+archivoarff.write(inicio)
+for l in listaAtributos:
+    archivoarff.write(listaAtributos[l])
+    archivoarff.write("\n")
+archivoarff.close()
+    
 names.close()
